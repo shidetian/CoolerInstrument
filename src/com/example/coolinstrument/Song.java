5d5c4bda4389eda7c5649240eb560f3ed4465f2d
Detian Shi <hi_cannon@yahoo.com>
2014-02-08 16:45:46 -0500

    Clone from previous repo

---
+++ src/com/example/coolinstrument/Song.java
@@ -0,0 +1,11 @@
+package com.example.coolinstrument;
+
+import java.util.TreeMap;
+
+public class Song {
+	TreeMap<Integer, Integer> mySong;
+	
+	public Song(){
+		 mySong = new TreeMap<Integer,Integer>();
+	}
+}
